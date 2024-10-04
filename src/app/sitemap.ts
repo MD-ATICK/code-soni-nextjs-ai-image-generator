bd35ac81121cd9140622599483ae8ea5b6b276cd
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-10-04 12:30:21 +0600

    done finally

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,16 +3,19 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url: '/',
-            priority: 1
+            url: 'https://imagealx.vercel.app/',
+            lastModified: new Date().toISOString(), // Current date for last modified
+            priority: 1,
         },
         {
-            url: '/generate',
-            priority: 0.8
+            url: 'https://imagealx.vercel.app/generate',
+            lastModified: new Date().toISOString(), // Current date for last modified
+            priority: 0.8,
         },
         {
-            url: '/profile',
-            priority: 0
+            url: 'https://imagealx.vercel.app/profile',
+            lastModified: new Date().toISOString(), // Current date for last modified
+            priority: 0.5, // Updated from 0 to 0.5
         },
-    ]
+    ];
 }

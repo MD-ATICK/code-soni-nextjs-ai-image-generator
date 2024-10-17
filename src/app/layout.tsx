f7e95a5401b423b493841676d1bcfe39be89b046
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-10-17 12:21:19 +0600

    seo edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     siteName: process.env.NEXT_WEB_NAME,
-    title: process.env.NEXT_WEB_NAME,
+    title: process.env.NEXT_WEB_TITLE,
     description: process.env.NEXT_WEB_DESCRIPTION,
     url: ` ${process.env.NEXT_DEPLOY_WEB_URL}`,
     type: 'website',
@@ -47,7 +47,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: process.env.NEXT_WEB_NAME,
+    title: process.env.NEXT_WEB_TITLE,
     description: process.env.NEXT_WEB_DESCRIPTION,
     images: [`${process.env.NEXT_DEPLOY_WEB_URL}/twitter.jpg`],
   },
